f413980e3ee180138721c687f74b4e6cc1647aa2
Nimey1782 <nimeykhurana44@gmail.com>
2026-07-26 13:54:08 +0530

    Initial project setup

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.database import Base
+
+#creating a table User -> in more technical terms -> this python class represents a table user
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True)
+
